3b81906255443183624d45616df30e7c71e006fd
kirdol <lodrik.adam@hotmail.ch>
2024-04-29 13:45:30 +0200

    adding Setup.R
    -ajout de setup.R
    -ajout de dossiers

---
+++ scripts/setup.R
@@ -0,0 +1,17 @@
+# load the required packages and install them if they are not.
+packages_loaded <- c(
+  
+)
+
+# Function that install the packages if not already installed on your computer
+for (pkg in packages_loaded) {
+  if (!pkg %in% installed.packages()) {
+    install.packages(pkg)}}
+
+# load the packages
+for (pkg in packages_loaded) {
+  library(pkg, character.only = TRUE)}
+
+# cleaning of the environment
+rm(pkg)
+
